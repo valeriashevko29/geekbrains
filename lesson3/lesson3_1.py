4fe69c5ccf74db147e7044e7cbfb281f06a44cf3
Valeria Shevko <valeriashevko29@gmail.com>
2022-05-08 15:27:14 +0300

    lesson 3.1 modified with try-except

--- lesson3/lesson3_1.py
+++ lesson3/lesson3_1.py
@@ -2,3 +2,20 @@
 # и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 
+def my_f (a, b):
+    # a = int(input('Enter first number: '))
+    # b = int(input('Enter second number: '))
+    try:
+        a = int(a)
+    except ValueError as error2:
+        print(error2)
+    try:
+        b = int(b)
+    except ValueError as error3:
+        print(error3)
+    try:
+        return a // b
+    except (ZeroDivisionError, TypeError) as error1:
+        print(error1)
+
+print(my_f(input('Enter first number: '), input('Enter second number: ')))
