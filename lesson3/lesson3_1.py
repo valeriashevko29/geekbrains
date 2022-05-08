2ad5d702e54f70d6b9b798fd72657e03d65446db
Valeria Shevko <valeriashevko29@gmail.com>
2022-05-08 15:42:12 +0300

    lesson 3.1 modified with all try-except

--- lesson3/lesson3_1.py
+++ lesson3/lesson3_1.py
@@ -3,19 +3,18 @@
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 
 def my_f (a, b):
-    # a = int(input('Enter first number: '))
-    # b = int(input('Enter second number: '))
     try:
         a = int(a)
-    except ValueError as error2:
-        print(error2)
+    except ValueError:
+        print("The first value is not a number")
     try:
         b = int(b)
-    except ValueError as error3:
-        print(error3)
+    except ValueError:
+        print("The second value is not a number")
     try:
         return a // b
-    except (ZeroDivisionError, TypeError) as error1:
-        print(error1)
+    except (TypeError, ZeroDivisionError) as err:
+        print("Operation is impossible")
+        print(err)
 
 print(my_f(input('Enter first number: '), input('Enter second number: ')))
