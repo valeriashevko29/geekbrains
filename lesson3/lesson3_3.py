91d8c38ac1276570d9d975d439feb4152bb6d56a
Valeria Shevko <valeriashevko29@gmail.com>
2022-05-08 16:43:26 +0300

    lesson 3.3 modified

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -1,3 +1,14 @@
 # Реализовать функцию my_func(),
 # которая принимает три позиционных аргумента и возвращает сумму наибольших двух аргументов.
+#
+def my_func(a, b, c):
+    list1 = [a, b, c]
+    list1.remove(min(list1))
+    list1 = sum(list1)
+    return print(list1)
+
+my_func(int(input()), int(input()), int(input()))
+
+
+
 
