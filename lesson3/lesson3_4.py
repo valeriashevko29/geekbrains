ed2afadf6f2cddc92d785964522c1e4c1a0510c0
Valeria Shevko <valeriashevko29@gmail.com>
2022-05-08 16:55:07 +0300

    lesson 3.4 modified

--- lesson3/lesson3_4.py
+++ lesson3/lesson3_4.py
@@ -2,3 +2,24 @@
 # отрицательное число y. Выполните возведение числа x в степень y.
 # Задание реализуйте в виде функции my_func(x, y).
 # При решении задания нужно обойтись без встроенной функции возведения числа в степень.
+
+def my_func(x, y):
+    try:
+        x = int(x)
+    except ValueError:
+        print("The first value is not a number")
+        return
+    try:
+        y = int(y)
+    except ValueError:
+        print("The second value is not a number")
+        return
+    if y > 0:
+        y = -y
+    return print(round((x ** y), 3))
+
+my_func(input(), input())
+
+
+
+
